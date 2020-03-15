c1962ca6ff45100f85f290403772fc684f118a81
LuisLiraC <luis_ssbm@hotmail.com>
2020-03-15 11:59:03 -0600

    First commit Platzi scraper

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const cors = require('cors')
+const scrap = require('./scrap')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('To get your courses information use /your_account. For example /Luis_LiraC')
+})
+
+app.get('/:account', async (req, res) => {
+  try {
+    const { account } = req.params
+    const data = await scrap(account)
+    res.status(200).json(data)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.listen(3000, () => {
+  console.log(`http://localhost:${PORT}`)
+})
---
+++ scrap.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer')
+
+async function getCoursesData(account) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  try {
+    await page.goto(`https://platzi.com/@${account}`)
+    const data= await page.evaluate(() => {
+      function mapData(data) {
+        return data.map(d => {
+          return {
+            id: d.id,
+            title: d.title,
+            logo: d.logo,
+            badge: d.badge,
+            diploma_link: `https://platzi.com${d.diploma_link}`
+          }
+        })
+      }
+
+      return data = {
+        careers: mapData(window.data.careers),
+        courses: [
+          ...mapData(window.data.courses),
+          ...mapData(window.data.deprecated)
+        ]
+      }
+    })
+
+    return data
+
+  } catch (error) {
+    browser.close()
+    console.log(error.message)
+    return {
+      error: 'This account is private or does not exists. To get your courses information go to your profile configuration in Platzi and set your account as public.'
+    }
+  }
+}
+
+module.exports = getCoursesData;
